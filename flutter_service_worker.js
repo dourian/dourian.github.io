ef926d4acf308b164fcf502b31a719abd2c2f652
dourian <75771131+dourian@users.noreply.github.com>
2022-07-07 19:40:44 -0400

    overhaul

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f33b42475e5befec0e6d955f6d806f33",
-"index.html": "710491b336d3d24f70a43c88d4637692",
-"/": "710491b336d3d24f70a43c88d4637692",
-"main.dart.js": "02035c3bf3c9d963b882d61bb1a2a585",
+"index.html": "2887804fe08d7f3510128c3af1f7111e",
+"/": "2887804fe08d7f3510128c3af1f7111e",
+"main.dart.js": "def2ee10666201b91c13ffeb9d444eb0",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
